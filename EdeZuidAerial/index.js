106762ff8eec63bceb684b53d656d5272f2e3d90
Erwin Kerk <erwin@mac-mini-m1.kpn>
2022-08-07 22:56:54 +0200

    added Ede Zuid Aerial

---
+++ EdeZuidAerial/index.js
@@ -0,0 +1,47 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66);"
+    },
+"include": [
+        {
+            "src": "index_controls.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "restart": 5,
+	        "status": "off",
+	        "x": 0.02,
+	        "y": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 13.52844355085196,
+	        "maxtilt": 2.8472777706422576,
+	        "minpan": 0,
+	        "mintilt": -2.736754964644642
+	    },
+	"id": "index",
+	"image":{"baseindex":1,"bitmap":[{"height":3423,"src":"index_tiles/index_t_1_%0y_%0x.jpg","tilesize":480,"width":8294},{"height":1712,"src":"index_tiles/index_t_2_%0y_%0x.jpg","tilesize":480,"width":4147},{"height":856,"src":"index_tiles/index_t_3_%0y_%0x.jpg","tilesize":480,"width":2074},{"height":428,"src":"index_tiles/index_t_4_%0y_%0x.jpg","tilesize":480,"width":1037}],"multilevel":true,"preview":{"src":"index_tiles/index_preview.jpg"},"projection":"planar"},
+	"textbox": [
+	        {
+	            "align": "top",
+	            "style": " color:#ffffff; font-family: Arial,Helvetica,sans-serif; text-shadow: 0px 0px 3.8px #000000, 0px 0px 2.5px #000000, 0px 0px 1.7px #000000; font-size: 12px; font-weight: bold; text-align:center;",
+	            "text": "EdeZuid - Aerial",
+	            "yoff": "4px"
+	        }
+	    ],
+	"view": {
+	        "mode": "fitInWindow"
+	    }
+},
+"version": "4.1"
+}
+}
